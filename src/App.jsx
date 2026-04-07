da57c90522a9dbd4bf232a9308e6d339a265d27d
Jatin Siradhana <siradhanajatin@gmail.com>
2026-04-08 00:06:27 +0530

    setup the basic structure of the website's major components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 // import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar'
+import Hero from './components/hero'
+import Contact from './components/contact_us'
+import About from './components/about_us'
+import Predict from './components/predict'
 
 function App() {
 
@@ -8,6 +12,22 @@ function App() {
     <>
       <div className='navbar'>
         <Navbar />
+      </div>
+      
+      <div className='hero'>
+        <Hero />
+      </div>
+
+      <div className='predict'>
+        <Predict />
+      </div>
+
+      <div className='about'>
+        <About />
+      </div>
+
+      <div className='contact'>
+        <Contact />
       </div>
     </>
   )
